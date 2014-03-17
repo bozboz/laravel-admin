213de8d12822569da012adfd3cec4ece8a35bcc3
Dan Bower <dan.bower88@gmail.com>
2014-03-17 09:31:20 +0000

    Update "page" blade template
    - Remove comments
    - Change "extend" to "extends"

--- src/views/pages/page.blade.php
+++ src/views/pages/page.blade.php
@@ -1,11 +1,5 @@
-@extend('layouts.default')
+@extends('layouts.default')
 
 @section('main')
 <h2>{{ $page->title}}</h2>
 @stop
-
-// app/views/packages/admin/layouts/default.blade.php
-
-// app/view/layouts/default.blade.php
-
-// app/views/packages/admin/structure/header.blade.php
