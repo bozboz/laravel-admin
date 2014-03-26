4fcc7b3bf131a0e2312bf7e81701587f4354a397
Dan Bower <dan.bower88@gmail.com>
2014-03-26 10:10:01 +0000

    Update TestCase
    Ensure the database is in a consistent state before each test is
    executed

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,9 +1,21 @@
 <?php namespace Bozboz\Admin\Tests;
 
+use Artisan;
+
 class TestCase extends \TestCase
 {
-	// public function setUp()
-	// {
-		
-	// }
+	public function setUp()
+	{
+		parent::setUp();
+		$this->prepareForTests();
+	}
+
+	private function prepareForTests()
+	{
+		$option = strpos(__DIR__, 'workbench') === false ? '--package' : '--bench';
+
+		Artisan::call('migrate:reset');
+		Artisan::call('migrate', array($option => 'bozboz/admin'));
+		Artisan::call('db:seed');
+	}
 }
