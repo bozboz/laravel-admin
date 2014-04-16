c3874ccdb53c62772867d62415a8e161727f2e05
Mike Street <mike.street@bozboz.co.uk>
2014-04-16 15:19:57 +0100

    Add sortable.js to overview and tweak styling
    Basic overview now uses lists instead of tables for use with the
    sortable.js. Updating CSS to include faux-table classes to keep
    styling consistent.

--- src/views/overview.blade.php
+++ src/views/overview.blade.php
@@ -5,28 +5,37 @@
 	@include('admin::partials.new')
 	<h1>{{ $modelName }}</h1>
 	<div class="table-responsive">
-		<table class="table table-striped">
-			<thead>
-			</thead>
+		<ol class="secret-list sortable faux-table">
 
 	@foreach ($instances as $i => $model)
-		<tr>
-		@foreach ($columns[$i] as $name => $value)
-			<td>{{ $value }}</td>
-		@endforeach
-			<td class="no-wrap">
-				<a href="{{ URL::action($controller . '@edit', array($model->id)) }}" class="btn btn-info btn-sm" type="submit">
-					<i class="fa fa-pencil"></i>
-					Edit
-				</a>
+			<li class="faux-table-row">
+				<div class="faux-cell cell-small">
+					<i class="fa fa-sort sorting-handle"></i>
+				</div>
+			@foreach ($columns[$i] as $name => $value)
+				<div class="faux-cell">{{ $value }}</div>
+			@endforeach
+				<div class="no-wrap faux-cell">
+					<a href="{{ URL::action($controller . '@edit', array($model->id)) }}" class="btn btn-info btn-sm" type="submit">
+						<i class="fa fa-pencil"></i>
+						Edit
+					</a>
 
-				{{ Form::model($model, array('class' => 'inline-form', 'action' => array($controller . '@destroy', $model->id), 'method' => 'DELETE')) }}
-					<button class="btn btn-danger btn-sm" type="submit"><i class="fa fa-minus-square"></i> Delete</button>
-				{{ Form::close() }}
-			</td>
-		</tr>
+					{{ Form::model($model, array('class' => 'inline-form', 'action' => array($controller . '@destroy', $model->id), 'method' => 'DELETE')) }}
+						<button class="btn btn-danger btn-sm" type="submit"><i class="fa fa-minus-square"></i> Delete</button>
+					{{ Form::close() }}
+				</div>
+			</li>
 	@endforeach
-	</table>
+	</ol>
 </div>
 	@include('admin::partials.new')
+@stop
+
+@section('scripts')
+    @parent
+<script src="/packages/bozboz/admin/js/jquery-sortable.js"></script>
+<script>
+$('.sortable').sortable();
+</script>
 @stop
