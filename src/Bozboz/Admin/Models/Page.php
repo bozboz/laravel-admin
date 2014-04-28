862ee2496cc2676f67822be39e5bce69bf4df48d
Sam King <sdking02@gmail.com>
2014-04-28 12:48:03 +0100

    Add support for parent/child pages in Page model

--- src/Bozboz/Admin/Models/Page.php
+++ src/Bozboz/Admin/Models/Page.php
@@ -5,7 +5,7 @@ use Bozboz\Admin\Models\Sortable;
 
 class Page extends Base implements Sortable
 {
-	protected $fillable = array('title', 'slug', 'description', 'template');
+	protected $fillable = array('title', 'slug', 'description', 'template', 'parent_id');
 
 	public function getValidator()
 	{
@@ -15,5 +15,20 @@ class Page extends Base implements Sortable
 	public function sortBy()
 	{
 		return 'sorting';
+	}
+
+	public function children()
+	{
+		return $this->hasMany(get_class($this), 'parent_id');
+	}
+
+	public function parent()
+	{
+		return $this->belongsTo(get_class($this));
+	}
+
+	public function scopeTopLevel()
+	{
+		return $this->where('parent_id', 0);
 	}
 }
