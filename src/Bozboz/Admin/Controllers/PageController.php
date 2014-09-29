98b63b8be3fdbc0ea82da2d6327d5e8582a2dca0
Sam King <sdking02@gmail.com>
2014-09-29 12:45:48 +0100

    PageController::show() method should throw a NotFoundHttpException
    Instead of an Eloquent ModelNotFoundException. This allows the app to
    handle it as a standard 404.

--- src/Bozboz/Admin/Controllers/PageController.php
+++ src/Bozboz/Admin/Controllers/PageController.php
@@ -2,18 +2,32 @@
 
 use Bozboz\Admin\Meta\Provider as Meta;
 use Bozboz\Admin\Models\Page;
-use View, Redirect;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use View, Redirect, Controller, Request;
 
-class PageController extends \Controller
+class PageController extends Controller
 {
+	/**
+	 * Lookup page by slug; and either render corresponding view, redirect to 
+	 * another page, or throw a NotFoundHttpException.
+	 *
+	 * @param  string  $slug
+	 * @throws Symfony\Component\HttpKernel\Exception\NotFoundHttpException
+	 * @return Symfony\Component\HttpFoundation\Response
+	 */
 	public function show($slug)
 	{
-		$page = Page::where('slug', $slug)->firstOrFail();
+		try {
+			$page = Page::where('slug', $slug)->firstOrFail();
 
-		if ($page->redirect_to_id) {
-			return $this->redirectTo(Page::findOrFail($page->redirect_to_id));
-		} else {
-			return $this->serveView($page);
+			if ($page->redirect_to_id) {
+				return $this->redirectTo(Page::findOrFail($page->redirect_to_id));
+			} else {
+				return $this->serveView($page);
+			}
+		} catch (ModelNotFoundException $e) {
+			throw new NotFoundHttpException('Page with slug "' . Request::path() . '" was not found.');
 		}
 	}
 
