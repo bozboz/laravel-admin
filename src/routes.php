2bc3f50940ed9fb5ee65598b079329114baaf3a5
Dan Bower <dan.bower88@gmail.com>
2014-12-11 13:29:07 +0000

    Refactor routes

--- src/routes.php
+++ src/routes.php
@@ -6,27 +6,23 @@
 |--------------------------------------------------------------------------
 */
 
-Route::group(array('namespace' => 'Bozboz\Admin\Controllers'), function() {
+Route::group(array('namespace' => 'Bozboz\Admin\Controllers', 'prefix' => 'admin'), function() {
 
-	Route::group(array('prefix' => 'admin'), function() {
+	View::composer('admin::partials.nav', 'Bozboz\Admin\Composers\Nav');
 
-		View::composer('admin::partials.nav', 'Bozboz\Admin\Composers\Nav');
+	Route::group(array('before' => 'auth'), function() {
+		Route::resource('pages', 'PageAdminController', array('except' => array('show')));
+		Route::resource('users', 'UserAdminController', array('except' => array('show')));
 
-		Route::group(array('before' => 'auth'), function() {
-			Route::resource('pages', 'PageAdminController', array('except' => array('show')));
-			Route::resource('users', 'UserAdminController', array('except' => array('show')));
+		Route::get('/', 'AdminController@getIndex');
+		Route::get('logout', 'AdminController@getLogout');
 
-			Route::get('/', 'AdminController@getIndex');
-			Route::get('logout', 'AdminController@getLogout');
+		Route::post('sort', 'SortController@sort');
+	});
 
-			Route::post('sort', 'SortController@sort');
-		});
-
-		Route::group(array('before' => 'guest'), function() {
-			Route::get('login', 'AdminController@getLogin');
-			Route::post('login', 'AdminController@postLogin');
-		});
-
+	Route::group(array('before' => 'guest'), function() {
+		Route::get('login', 'AdminController@getLogin');
+		Route::post('login', 'AdminController@postLogin');
 	});
 
 });
