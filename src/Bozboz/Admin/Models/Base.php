a5ac3f473affdae43642396cbb1b5e98d8e71557
Sam King <sdking02@gmail.com>
2015-01-21 17:15:52 +0000

    Move bulk of sanitiseInput logic into Base model

--- src/Bozboz/Admin/Models/Base.php
+++ src/Bozboz/Admin/Models/Base.php
@@ -15,4 +15,21 @@ abstract class Base extends Eloquent
 	 * @return Validator
 	 */
 	abstract public function getValidator();
+
+	/**
+	 * Sanitise form input, ready for database insertion
+	 *
+	 * @param  array  $input
+	 * @return array
+	 */
+	public function sanitiseInput($input)
+	{
+		foreach($this->nullable as $field) {
+			if (array_key_exists($field, $input)) {
+				$input[$field] = empty($input[$field]) ? null : $input[$field];
+			}
+		}
+
+		return $input;
+	}
 }
