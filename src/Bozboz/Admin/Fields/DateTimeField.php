bda365428ac9e305e5d89de974f2fda43bd547e0
Dan Bower <dan.bower88@gmail.com>
2015-02-17 17:08:55 +0000

    Wrap DateTimeField JS in anonymous function

--- src/Bozboz/Admin/Fields/DateTimeField.php
+++ src/Bozboz/Admin/Fields/DateTimeField.php
@@ -26,47 +26,49 @@ class DateTimeField extends Field
 		$jsonConfig = json_encode($this->configOptions);
 
 		return <<<JAVASCRIPT
-			var config = $jsonConfig;
+			(function() {
+				var config = $jsonConfig;
 
-			var dateTimePickerDefaults = {
-				showSecond: false,
-				dateFormat: 'dd/mm/yy',
-				altField: '#$this->name',
-				altFieldTimeOnly: false,
-				altFormat: 'yy-mm-dd',
-				altTimeFormat: 'HH:mm:ss',
-			};
+				var dateTimePickerDefaults = {
+					showSecond: false,
+					dateFormat: 'dd/mm/yy',
+					altField: '#$this->name',
+					altFieldTimeOnly: false,
+					altFormat: 'yy-mm-dd',
+					altTimeFormat: 'HH:mm:ss',
+				};
 
-			//Convert a MySQL DateTime formatted string into a JS Date object
-			var stringToDate = function(dateTimeString) {
-				var dateTimeExploded = dateTimeString.split(' ');
-				var dateInfo = dateTimeExploded[0].split('-');
-				var timeInfo = dateTimeExploded[1].split(':');
+				//Convert a MySQL DateTime formatted string into a JS Date object
+				var stringToDate = function(dateTimeString) {
+					var dateTimeExploded = dateTimeString.split(' ');
+					var dateInfo = dateTimeExploded[0].split('-');
+					var timeInfo = dateTimeExploded[1].split(':');
 
-				return new Date(dateInfo[0], dateInfo[1] - 1, dateInfo[2], timeInfo[0], timeInfo[1], timeInfo[2]);
-			};
+					return new Date(dateInfo[0], dateInfo[1] - 1, dateInfo[2], timeInfo[0], timeInfo[1], timeInfo[2]);
+				};
 
-			var dateTime = $('#$this->name').val() === '' ? null : stringToDate($('#$this->name').val())
+				var dateTime = $('#$this->name').val() === '' ? null : stringToDate($('#$this->name').val())
 
-			//Parse config and convert applicable strings to Date objects
-			var dateTimeFields = ['minDateTime', 'maxDateTime', 'defaultDateTime'];
-			for (var i = 0; i < dateTimeFields.length; i++) {
-				var j = dateTimeFields[i];
-				if (config[j] !== undefined && config[j] !== null) {
-					config[j] = new Date(config[j] * 1000);
+				//Parse config and convert applicable strings to Date objects
+				var dateTimeFields = ['minDateTime', 'maxDateTime', 'defaultDateTime'];
+				for (var i = 0; i < dateTimeFields.length; i++) {
+					var j = dateTimeFields[i];
+					if (config[j] !== undefined && config[j] !== null) {
+						config[j] = new Date(config[j] * 1000);
+					}
 				}
-			}
 
-			//Revert to some default if applicable
-			if (dateTime === null && config.defaultDateTime !== undefined) {
-				dateTime = config.defaultDateTime;
-			}
+				//Revert to some default if applicable
+				if (dateTime === null && config.defaultDateTime !== undefined) {
+					dateTime = config.defaultDateTime;
+				}
 
-			//Initialise datetimepicker
-			$('#$this->altName').datetimepicker($.extend(dateTimePickerDefaults, config));
-			if (dateTime !== null) {
-				$('#$this->altName').datetimepicker('setDate', dateTime);
-			}
+				//Initialise datetimepicker
+				$('#$this->altName').datetimepicker($.extend(dateTimePickerDefaults, config));
+				if (dateTime !== null) {
+					$('#$this->altName').datetimepicker('setDate', dateTime);
+				}
+			})();
 JAVASCRIPT;
 	}
 }
