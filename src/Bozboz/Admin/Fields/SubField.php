81a1286376c4dd3d695aeac80f511ef7250126c1
Dan Bower <dan.bower88@gmail.com>
2015-05-20 14:48:55 +0100

    Add SubField

---
+++ src/Bozboz/Admin/Fields/SubField.php
@@ -0,0 +1,42 @@
+<?php namespace Bozboz\Admin\Fields;
+
+use Form;
+use View;
+
+class SubField extends Field
+{
+    protected $identifier;
+    protected $decorate;
+    protected $originalFieldName;
+
+    public function __construct($identifier, Field $field)
+    {
+        $this->identifier = $identifier;
+        $this->decorate = $field;
+        $this->originalFieldName = $field->name;
+
+        $field->name = $identifier . '[' . $field->name . ']';
+    }
+
+    public function getInput()
+    {
+        return $this->decorate->getInput();
+    }
+
+    public function getLabel()
+    {
+        return Form::label($this->originalFieldName, $this->decorate->label);
+    }
+
+    public function getErrors(\Illuminate\Support\ViewErrorBag $errors)
+    {
+        if ($errors->has($this->originalFieldName)) {
+            return '<p><strong>' . $errors->first($this->originalFieldName) . '</strong></p>';
+        }
+    }
+
+    public function render()
+    {
+        return View::make('admin::fields.field')->with(['field' => $this]);
+    }
+}
