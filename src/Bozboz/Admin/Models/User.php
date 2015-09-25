a7b86dcafc79f75752548181060936f45e180d0f
Sam King <sdking02@gmail.com>
2015-09-25 15:44:32 +0100

    Update User model as per L5 recomendation

--- src/Bozboz/Admin/Models/User.php
+++ src/Bozboz/Admin/Models/User.php
@@ -1,12 +1,20 @@
 <?php namespace Bozboz\Admin\Models;
 
-use Illuminate\Auth\UserInterface;
-use Illuminate\Auth\Reminders\RemindableInterface;
-use Hash;
 use Bozboz\Admin\Services\Validators\UserValidator;
+use Hash;
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Foundation\Auth\Access\Authorizable;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
-class User extends Base implements UserInterface, RemindableInterface
+class User extends Base implements AuthenticatableContract,
+                                   AuthorizableContract,
+                                   CanResetPasswordContract
 {
+    use Authenticatable, Authorizable, CanResetPassword;
+
 	/**
 	 * The Model validator
 	 *
