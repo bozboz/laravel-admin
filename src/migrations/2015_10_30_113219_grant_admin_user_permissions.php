247c96739f18c7237b9ff809d4ffeda9fd85176f
Sam King <sdking02@gmail.com>
2015-10-30 11:35:39 +0000

    Add migration to grant default user permissions

---
+++ src/migrations/2015_10_30_113219_grant_admin_user_permissions.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class GrantAdminUserPermissions extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		$users = DB::table('users')->select('id')->get();
+
+		foreach($users as $user) {
+			$this->grantWildCardPermissionForUser($user);
+		}
+	}
+
+	protected function grantWildCardPermissionForUser($user)
+	{
+		$now = new DateTime;
+
+		DB::table('permissions')->insert([
+			'user_id' => $user->id,
+			'action' => '*',
+			'created_at' => $now,
+			'updated_at' => $now
+		]);
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		DB::table('permissions')->truncate();
+	}
+
+}
