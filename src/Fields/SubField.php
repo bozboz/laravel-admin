dc6837e0dd1ab8bde438afed8f2a61c581b3a773
Sam King <sdking02@gmail.com>
2015-11-30 13:12:40 +0000

    Fix validation error output on SubField
    And add docblocks

--- src/Fields/SubField.php
+++ src/Fields/SubField.php
@@ -1,37 +1,107 @@
 <?php namespace Bozboz\Admin\Fields;
 
 use Form;
-use View;
+use Illuminate\Support\ViewErrorBag;
 
 class SubField extends Field
 {
+    /**
+     * The name of the group, identifying the subfield
+     *
+     * @var string
+     */
     protected $identifier;
+
+    /**
+     * The field in which to decorate
+     *
+     * @var Bozboz\Admin\Fields\Field
+     */
     protected $decorate;
+
+    /**
+     * The original decorated field name, for retrieval
+     *
+     * @var string
+     */
     protected $originalFieldName;
 
+    /**
+     * Rewrite the passed in field's name, to give it context in the group
+     *
+     * E.g. field_name => group[field_name]
+     *
+     * @param  string  $identifier
+     * @param  Bozboz\Admin\Fields\Field  $field
+     */
     public function __construct($identifier, Field $field)
     {
         $this->identifier = $identifier;
         $this->decorate = $field;
         $this->originalFieldName = $field->name;
 
-        $field->name = $identifier . '[' . $field->name . ']';
+        $field->name = $this->getFieldNameInBracketSyntax();
     }
 
+    /**
+     * Render the decorated field's input output
+     *
+     * @return string
+     */
     public function getInput()
     {
         return $this->decorate->getInput();
     }
 
+    /**
+     * Render a label for the sub field, using the decorated field's specified
+     * label if defined
+     *
+     * @return string
+     */
     public function getLabel()
     {
         return Form::label($this->originalFieldName, $this->decorate->label);
     }
 
-    public function getErrors(\Illuminate\Support\ViewErrorBag $errors)
+    /**
+     * Render output of errors for the sub field
+     *
+     * @param  Illuminate\Support\ViewErrorBag  $errors
+     * @return string
+     */
+    public function getErrors(ViewErrorBag $errors)
     {
-        if ($errors->has($this->decorate->name)) {
-            return '<p><strong>' . $errors->first($this->decorate->name) . '</strong></p>';
+        $key = $this->getFieldNameInDotSyntax();
+
+        if ($errors->has($key)) {
+            return '<p><strong>' . $errors->first($key) . '</strong></p>';
         }
+    }
+
+    /**
+     * Get the name of the subfield in the field group using bracket syntax, for
+     * form inputs
+     *
+     * Example output: group[sub_field]
+     *
+     * @return string
+     */
+    protected function getFieldNameInBracketSyntax()
+    {
+        return $this->identifier . '[' . $this->originalFieldName . ']';
+    }
+
+    /**
+     * Get the name of the subfield in the field group using dot syntax, for
+     * checking error bags
+     *
+     * Example output: group.sub_field
+     *
+     * @return string
+     */
+    protected function getFieldNameInDotSyntax()
+    {
+        return $this->identifier . '.' . $this->originalFieldName;
     }
 }
