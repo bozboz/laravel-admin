a0d8d46e09ee0a082772942a5cd165754c8791b9
Sam King <sdking02@gmail.com>
2015-12-02 15:05:43 +0000

    Prevent error when using SanitisesInputTrait
    Previously, if the class didn't define a `$nullable` property, an error
    would occur

--- src/Traits/SanitisesInputTrait.php
+++ src/Traits/SanitisesInputTrait.php
@@ -12,12 +12,22 @@ trait SanitisesInputTrait
 	 */
 	public function sanitiseInput($input)
 	{
-		foreach($this->nullable as $field) {
+		foreach($this->getNullableAttributes() as $field) {
 			if (array_key_exists($field, $input)) {
 				$input[$field] = empty($input[$field]) ? null : $input[$field];
 			}
 		}
 
 		return $input;
+	}
+
+	/**
+	 * Get nullable properties defined on class, or fall back to an empty array
+	 *
+	 * @return array
+	 */
+	protected function getNullableAttributes()
+	{
+		return property_exists($this, 'nullable') ? (array) $this->nullable : [];
 	}
 }
