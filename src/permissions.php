a12b3e36035db18e29e8c37509d550c67fd68b2f
Sam King <sdking02@gmail.com>
2015-12-04 14:23:16 +0000

    Convert Rules to ModelRules

--- src/permissions.php
+++ src/permissions.php
@@ -7,18 +7,18 @@ $permissions->define([
 	'edit_profile'    => 'Bozboz\Admin\Permissions\UserRule',
 
 	'view_anything'   => 'Bozboz\Permissions\Rules\GlobalRule',
-	'create_anything' => 'Bozboz\Permissions\Rules\Rule',
-	'edit_anything'   => 'Bozboz\Permissions\Rules\Rule',
-	'delete_anything' => 'Bozboz\Permissions\Rules\Rule',
+	'create_anything' => 'Bozboz\Permissions\Rules\ModelRule',
+	'edit_anything'   => 'Bozboz\Permissions\Rules\ModelRule',
+	'delete_anything' => 'Bozboz\Permissions\Rules\ModelRule',
 
 	'view_users'   => 'Bozboz\Permissions\Rules\GlobalRule',
 	'create_user'  => 'Bozboz\Permissions\Rules\GlobalRule',
-	'edit_user'    => 'Bozboz\Permissions\Rules\Rule',
-	'delete_user'  => 'Bozboz\Permissions\Rules\Rule',
+	'edit_user'    => 'Bozboz\Permissions\Rules\ModelRule',
+	'delete_user'  => 'Bozboz\Permissions\Rules\ModelRule',
 
 	'view_media'   => 'Bozboz\Permissions\Rules\GlobalRule',
 	'create_media' => 'Bozboz\Permissions\Rules\GlobalRule',
-	'edit_media'   => 'Bozboz\Permissions\Rules\Rule',
-	'delete_media' => 'Bozboz\Permissions\Rules\Rule',
+	'edit_media'   => 'Bozboz\Permissions\Rules\ModelRule',
+	'delete_media' => 'Bozboz\Permissions\Rules\ModelRule',
 
 ]);
