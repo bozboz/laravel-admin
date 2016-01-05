593b6c943462725afed8192c8e507d7978e6517a
Jim Hollington <jim@bozboz.co.uk>
2016-01-05 17:10:52 +0000

    Move date field js to bower scripts

--- src/Fields/DateField.php
+++ src/Fields/DateField.php
@@ -1,71 +1,29 @@
 <?php namespace Bozboz\Admin\Fields;
 
 use Form;
+use Illuminate\Support\Str;
 
 class DateField extends Field
 {
-	private $altName;
-
-	protected $configOptions;
-
-	public function __construct($attributes)
+	protected function getJsClass()
 	{
-		parent::__construct($attributes);
-
-		$this->altName = $this->name . '_alt';
-		$this->configOptions = empty($this->options) ? [] : $this->options;
+		return 'js-datepicker';
 	}
 
 	public function getInput()
 	{
-		return sprintf('<input type="text" id="%s">%s', $this->altName, Form::hidden($this->name));
+		return Form::text($this->name, null, [
+			'id' => $this->name,
+			'class' => 'form-control form-control--small '.$this->getJsClass()
+		]);
 	}
 
 	public function getJavascript()
 	{
-		$jsonConfig = json_encode($this->configOptions);
+		$jsonConfig = json_encode($this->options ?: []);
 
 		return <<<JAVASCRIPT
-			(function() {
-				var config = $jsonConfig;
-
-				var datePickerDefaults = {
-					showSecond: false,
-					dateFormat: 'dd/mm/yy',
-					altField: '#$this->name',
-					altFieldTimeOnly: false,
-					altFormat: 'yy-mm-dd',
-				};
-
-				//Convert a MySQL DateTime formatted string into a JS Date object
-				var stringToDate = function(dateString) {
-					var dateInfo = dateString.split('-');
-
-					return new Date(dateInfo[0], dateInfo[1] - 1, dateInfo[2]);
-				};
-
-				var date = $('#$this->name').val() === '' ? null : stringToDate($('#$this->name').val())
-
-				//Parse config and convert applicable strings to Date objects
-				var dateFields = ['defaultDate'];
-				for (var i = 0; i < dateFields.length; i++) {
-					var j = dateFields[i];
-					if (config[j] !== undefined && config[j] !== null) {
-						config[j] = new Date(config[j] * 1000);
-					}
-				}
-
-				//Revert to some default if applicable
-				if (date === null && config.defaultDate !== undefined) {
-					date = config.defaultDate;
-				}
-
-				//Initialise datepicker
-				$('#$this->altName').datepicker($.extend(datePickerDefaults, config));
-				if (date !== null) {
-					$('#$this->altName').datepicker('setDate', date);
-				}
-			})();
+			window.datePicker = window.datePicker || {};
+			window.datePicker.{$this->name} = $jsonConfig;
 JAVASCRIPT;
-	}
-}
+	}}
--- src/Fields/DateTimeField.php
+++ src/Fields/DateTimeField.php
@@ -1,76 +1,11 @@
 <?php namespace Bozboz\Admin\Fields;
 
 use Form;
-use Illuminate\Support\Str;
 
-class DateTimeField extends Field
+class DateTimeField extends DateField
 {
-	private $altName, $sanitisedName;
-
-	protected $configOptions;
-
-	public function __construct($attributes)
+	protected function getJsClass()
 	{
-		parent::__construct($attributes);
-
-		$this->sanitisedName = Str::slug($this->name);
-		$this->altName = $this->sanitisedName . '_alt';
-		$this->configOptions = empty($this->options) ? [] : $this->options;
-	}
-
-	public function getInput()
-	{
-		return sprintf('<input type="text" id="%s">%s', $this->altName, Form::hidden($this->name, null, ['id' => $this->sanitisedName]));
-	}
-
-	public function getJavascript()
-	{
-		$jsonConfig = json_encode($this->configOptions);
-
-		return <<<JAVASCRIPT
-			(function() {
-				var config = $jsonConfig;
-
-				var dateTimePickerDefaults = {
-					showSecond: false,
-					dateFormat: 'dd/mm/yy',
-					altField: '#$this->sanitisedName',
-					altFieldTimeOnly: false,
-					altFormat: 'yy-mm-dd',
-					altTimeFormat: 'HH:mm:ss',
-				};
-
-				//Convert a MySQL DateTime formatted string into a JS Date object
-				var stringToDate = function(dateTimeString) {
-					var dateTimeExploded = dateTimeString.split(' ');
-					var dateInfo = dateTimeExploded[0].split('-');
-					var timeInfo = dateTimeExploded[1].split(':');
-
-					return new Date(dateInfo[0], dateInfo[1] - 1, dateInfo[2], timeInfo[0], timeInfo[1], timeInfo[2]);
-				};
-
-				var dateTime = $('#$this->sanitisedName').val() === '' ? null : stringToDate($('#$this->sanitisedName').val())
-
-				//Parse config and convert applicable strings to Date objects
-				var dateTimeFields = ['minDateTime', 'maxDateTime', 'defaultDateTime'];
-				for (var i = 0; i < dateTimeFields.length; i++) {
-					var j = dateTimeFields[i];
-					if (config[j] !== undefined && config[j] !== null) {
-						config[j] = new Date(config[j] * 1000);
-					}
-				}
-
-				//Revert to some default if applicable
-				if (dateTime === null && config.defaultDateTime !== undefined) {
-					dateTime = config.defaultDateTime;
-				}
-
-				//Initialise datetimepicker
-				$('#$this->altName').datetimepicker($.extend(dateTimePickerDefaults, config));
-				if (dateTime !== null) {
-					$('#$this->altName').datetimepicker('setDate', dateTime);
-				}
-			})();
-JAVASCRIPT;
+		return 'js-datetimepicker';
 	}
 }
