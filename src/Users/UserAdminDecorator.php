6387b6a0c8bc5ec8a1f2a9e5d4d260c2d58905ac
Jim Hollington <jim@bozboz.co.uk>
2016-01-06 14:14:01 +0000

    Fallback to email for user label if they have no name set

--- src/Users/UserAdminDecorator.php
+++ src/Users/UserAdminDecorator.php
@@ -38,7 +38,7 @@ class UserAdminDecorator extends ModelAdminDecorator
 
 	public function getLabel($instance)
 	{
-		return $instance->first_name . ' ' . $instance->last_name;
+		return $instance->first_name ? $instance->first_name . ' ' . $instance->last_name : $instance->email;
 	}
 
 	public function getFields($instance)
