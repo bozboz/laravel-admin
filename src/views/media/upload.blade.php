2eadc6d6d4d06087f53acfc3908746a16137986b
Jim Hollington <jim@bozboz.co.uk>
2016-01-18 16:03:03 +0000

    Fix media uploader is_private boolean

--- src/views/media/upload.blade.php
+++ src/views/media/upload.blade.php
@@ -68,11 +68,11 @@
 			<strong class="error text-danger"></strong>
 		</td>
 		<td>
-			<input name="caption[]" placeholder="Caption" class="caption form-control">
+			<input name="caption" placeholder="Caption" class="caption form-control">
 		</td>
 		<td>
 			<label>Private
-				<input name="is_private[]" type="checkbox" class="is-private form-control">
+				<input name="is_private" type="checkbox" class="is-private form-control">
 			</label>
 		</td>
 		<td>
