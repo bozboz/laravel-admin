3cebaf4208b8dddbfb969bb695d4966a999c9c4c
Jim Hollington <jim@bozboz.co.uk>
2016-01-21 17:49:18 +0000

    Remove unnecessary indentind

--- resources/views/report-actions/link.blade.php
+++ resources/views/report-actions/link.blade.php
@@ -1,8 +1,8 @@
-	<a{!! HTML::attributes([
-		'href' => $url,
-		'class' => $class . ' btn btn-sm',
-		'data-warn' => $warn
-	]) !!}>
-		<i class="{{ $icon }}"></i>
-		{{ $label }}
-	</a>
+<a{!! HTML::attributes([
+	'href' => $url,
+	'class' => $class . ' btn btn-sm',
+	'data-warn' => $warn
+]) !!}>
+	<i class="{{ $icon }}"></i>
+	{{ $label }}
+</a>
