b52a5268ec12a592bddb640bdbf523adee9c539b
Sam King <sdking02@gmail.com>
2016-04-22 14:56:44 +0100

    Change HiddenFilter to be a decorator for filters

--- src/Reports/Filters/HiddenFilter.php
+++ src/Reports/Filters/HiddenFilter.php
@@ -2,8 +2,22 @@
 
 namespace Bozboz\Admin\Reports\Filters;
 
+use Illuminate\Database\Eloquent\Builder;
+
 class HiddenFilter extends ListingFilter
 {
+	protected $filter;
+
+	public function __construct(ListingFilter $filter)
+	{
+		$this->filter = $filter;
+	}
+
+	public function filter(Builder $builder)
+	{
+		$this->filter->filter($builder);
+	}
+
 	public function __toString()
 	{
 		return '';
