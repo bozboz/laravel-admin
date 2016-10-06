55417f238968af4c5783b7da8d62a5ab75496e25
Jim Hollington <jim@bozboz.co.uk>
2016-10-06 10:12:42 +0100

    Add role to user storeRules

--- src/Services/Validators/UserValidator.php
+++ src/Services/Validators/UserValidator.php
@@ -1,15 +1,17 @@
-<?php namespace Bozboz\Admin\Services\Validators;
+<?php
+
+namespace Bozboz\Admin\Services\Validators;
 
 class UserValidator extends Validator
 {
-	protected $rules = array(
+	protected $rules = [
 		'email' => 'required|email|unique:users,email,{id}',
 		'first_name' => 'required',
 		'last_name' => 'required',
-		'role' => 'required',
-	);
+    ];
 
-	protected $storeRules = array(
+    protected $storeRules = [
+		'role_id' => 'required',
 		'password' => 'required|min:8',
-	);
+	];
 }
