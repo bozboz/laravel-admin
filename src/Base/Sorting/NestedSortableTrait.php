4430c6e7a1ceedcd769447ecae37acf71419e190
Jim Hollington <jim@bozboz.co.uk>
2016-10-19 16:37:46 +0100

    Set DB to strict mode and check errors when sorting

--- src/Base/Sorting/NestedSortableTrait.php
+++ src/Base/Sorting/NestedSortableTrait.php
@@ -2,6 +2,7 @@
 
 namespace Bozboz\Admin\Base\Sorting;
 
+use Illuminate\Database\QueryException;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use LogicException;
@@ -25,6 +26,12 @@ trait NestedSortableTrait
 	public function sort($before, $after, $parent)
 	{
 		try {
+			// The nested set package has a bug when trying to move a node to
+			// where it already is causing it to set the _lft and _rgt values to
+			// null. Set the DB mode to strict so that it won't allow setting
+			// the columns to null.
+			DB::statement("SET SESSION sql_mode = 'STRICT_TRANS_TABLES';");
+
 			DB::beginTransaction();
 
 			// if the node has a sibling before it, insert after it
@@ -63,6 +70,18 @@ trait NestedSortableTrait
 			} else {
 				Log::error('Nested sort error (ABORT)');
 
+				throw $e;
+			}
+		}
+		catch (QueryException $e) {
+			DB::rollback();
+
+			// The nested set package has a bug when trying to move a node to
+			// where it already is causing it to set the _lft and _rgt values to
+			// null. If they're both null here then that's likely what's
+			// happened so don't need to care.
+			// If they're not null then who knows what happened, rethrow.
+			if ( ! is_null($this->_lft) && ! is_null($this->_rgt)) {
 				throw $e;
 			}
 		}
